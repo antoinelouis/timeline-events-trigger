94c4f52cb50683a3e081c49b60cad61e02c5425c
antoinelouis <contact@antoinelouis.com>
2018-04-21 01:19:37 +0200

    simulate hover

--- demo/source/javascripts/site.js
+++ demo/source/javascripts/site.js
@@ -1,23 +1,32 @@
 //= require 'smooth-scroll/dist/js/smooth-scroll'
 
 document.addEventListener("DOMContentLoaded", function(event) { 
+
   const timeline = new TimelineEventsTrigger;
   var clickTarget = document.querySelector('.btn');
 
+  clickTarget.addEventListener('mouseenter', function(e){
+    console.log(e);
+    console.dir(e);
+  });
+  clickTarget.addEventListener('mousemove', function(e){
+    console.log(e);
+    console.dir(e);
+  });
   clickTarget.addEventListener('click', function(e){
     e.preventDefault();
     console.log('click');
     e.target.innerHTML += " clicked";
   })
 
-  timeline.addKey("click", 4000, {
-    target: clickTarget,
-    duration: 1000
+
+  timeline.addKey("scroll", 1000, {
+    scrollTo: 100,
+    duration: 500
   })
 
-  timeline.addKey("scroll", 2000, {
-    scrollTo: 400,
-    duration: 4000
+  timeline.addKey("hover", 1000, {
+    target: clickTarget
   })
 
   if (window.confirm("Trigger events sequence?")) { 
@@ -64,16 +73,43 @@ TimelineEventsTrigger.prototype.Key.prototype.executeKey = function(cb, index){
     console.log(eventtype);
     switch (eventtype){
       case 'click':
-        var newEvent =  new MouseEvent("click", {
+        var click =  new MouseEvent("click", {
           bubbles: true,
           cancelable: false,
           view: window
         });
-        options.target.dispatchEvent(newEvent);
+        options.target.dispatchEvent(click);
         break;
       case 'scroll':
         var scroll = new SmoothScroll();
         scroll.animateScroll( options.scrollTo, null, {speed: options.duration});
+        break;
+      case 'hover':
+        var hover =  new MouseEvent("mouseenter", {
+          bubbles: true,
+          cancelable: true,
+          relatedTarget: document.querySelector('div.hero'),
+        });
+        options.target.dispatchEvent(hover);
+
+        function getCssPropertyForRule(rule) {
+          var sheets = document.styleSheets;
+          var slen = sheets.length;
+          for(var i=0; i<slen; i++) {
+            var rules = sheets[i].cssRules;
+            var rlen = rules.length;
+            for(var j=0; j<rlen; j++) {
+              if(rules[j].selectorText == rule) {
+                return rules[j].style;
+              }
+            }
+          }
+        }
+
+        var foo = getCssPropertyForRule(options.target.tagName.toLowerCase()+':hover');
+        var cssAttr = foo[0];
+        options.target.style[cssAttr] = foo[cssAttr];
+
         break;
     }
     cb.executeTimeline(index);
